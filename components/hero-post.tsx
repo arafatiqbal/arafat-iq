3e9c59feea5316d0c0056d31ed9f240c0dd78d7f
arafatiqbal <arafatiqbal123@gmail.com>
2022-12-25 16:10:39 -0800

    Initial commit

--- components/hero-post.tsx
+++ components/hero-post.tsx
@@ -1,17 +1,17 @@
-import Avatar from './avatar'
-import DateFormatter from './date-formatter'
-import CoverImage from './cover-image'
-import Link from 'next/link'
-import type Author from '../interfaces/author'
+import Avatar from "./avatar";
+import DateFormatter from "./date-formatter";
+import CoverImage from "./cover-image";
+import Link from "next/link";
+import type Author from "../interfaces/author";
 
 type Props = {
-  title: string
-  coverImage: string
-  date: string
-  excerpt: string
-  author: Author
-  slug: string
-}
+  title: string;
+  coverImage: string;
+  date: string;
+  excerpt: string;
+  author: Author;
+  slug: string;
+};
 
 const HeroPost = ({
   title,
@@ -23,10 +23,10 @@ const HeroPost = ({
 }: Props) => {
   return (
     <section>
-      <div className="mb-8 md:mb-16">
-        <CoverImage title={title} src={coverImage} slug={slug} />
-      </div>
       <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
+        <div className="w-100 mb-8 md:mb-16">
+          <CoverImage title={title} src={coverImage} slug={slug} />
+        </div>
         <div>
           <h3 className="mb-4 text-4xl lg:text-5xl leading-tight">
             <Link
@@ -37,17 +37,13 @@ const HeroPost = ({
               {title}
             </Link>
           </h3>
-          <div className="mb-4 md:mb-0 text-lg">
-            <DateFormatter dateString={date} />
+          <div>
+            <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
           </div>
-        </div>
-        <div>
-          <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-          <Avatar name={author.name} picture={author.picture} />
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default HeroPost
+export default HeroPost;
